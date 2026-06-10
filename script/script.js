a192457546bec1fc3786f1bac94742f508d78a3d
Illegal_Empanada <158210860+IllegalEmpanada@users.noreply.github.com>
2026-06-09 23:25:55 -0300

    Add files :3
    :3

---
+++ script/script.js
@@ -0,0 +1,7 @@
+document.querySelector("#cutebutton").onclick = function () {
+	alert("youre such a good girl :3")
+};
+
+document.querySelector("#boymoder").onclick = function () {
+	alert("why are you clicking me?! >///<")
+}
